6041f515fc4ba1aee7c43b366ec213eef2a2eaa8
JEPerezGomez <82849361+JEPerezGomez@users.noreply.github.com>
2021-10-05 12:26:16 -0400

    Constructor de oraciones
    Buenas tardes profesor, aquí anexo el programa del constructor de oraciones.
    Saludos.

---
+++ Constructor de oraciones.cs
@@ -0,0 +1,21 @@
+﻿using System;
+	
+
+	namespace lenovo
+{
+	class program{
+
+	static void Main(string[] args){
+		string a,b,c, miCadena;
+		Console.WriteLine("Bienvenido al escritor de oraciones"+"\n"+"Este escritor funciona con un maximo de 3 palabras");
+		Console.WriteLine("\n"+"Ingrese la primera palabra:");
+		a=Console.ReadLine();
+		Console.WriteLine("\n"+"Ingrese la segunda palabra:");
+		b=Console.ReadLine();
+		Console.WriteLine("\n"+"Ingrese la tercera palabra:");
+		c=Console.ReadLine();
+		miCadena=a+""+b+""+""+c;
+		Console.WriteLine("\n"+"La oracion construida es:"+miCadena);
+	}    
+
+}}
